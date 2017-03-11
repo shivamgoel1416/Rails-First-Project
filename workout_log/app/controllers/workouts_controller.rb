128da43f3199ffc7de3538661030aa3e4f509e09
shivamgoel1416 <goel.shivam@tcs.com>
2017-03-11 10:57:21 +0900

    WorkOutLog first commit

---
+++ workout_log/app/controllers/workouts_controller.rb
@@ -0,0 +1,46 @@
+class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    if @workout.update(workout_params)
+      redirect_to root_url flash[:notice] = "Successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @workout.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:date,:workout,:mood,:length)
+  end
+end
