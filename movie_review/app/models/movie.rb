127aba404c0e317e2d39ba5e98c483fbd5eb6bb1
shivamgoel1416 <goel.shivam@tcs.com>
2017-03-16 16:02:39 +0900

    movie review starring completed

--- movie_review/app/models/movie.rb
+++ movie_review/app/models/movie.rb
@@ -3,4 +3,5 @@ class Movie < ApplicationRecord
   mount_uploader :image, ImageUploader
   # has_attached_file :image, styles: { medium: "400x600#>"}
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_many :reviews, dependent: :destroy
 end
--- movie_review/db/schema.rb
+++ movie_review/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314115509) do
+ActiveRecord::Schema.define(version: 20170316032644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20170314115509) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
