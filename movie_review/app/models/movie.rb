3e82b53729f1cc716a2b830cdba32dec10ab6bd1
shivamgoel1416 <goel.shivam@tcs.com>
2017-03-16 12:04:52 +0900

    Movies Review first commit

---
+++ movie_review/app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  belongs_to :user
+  mount_uploader :image, ImageUploader
+  # has_attached_file :image, styles: { medium: "400x600#>"}
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
